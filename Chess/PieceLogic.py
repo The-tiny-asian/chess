c054735d0d1c66cd7b2b48bb5dc95b4da74bcdd3
Rheemster <31289850+Rheemster@users.noreply.github.com>
2018-03-12 13:25:48 -0600

    READ THIS
    So the get functions (getTeam() and getPiece()) have been removed.  Instead, if you need to reference a variable from an object, you can just use object.variable.  So tile.getTeam() is now tile.isWhite and tile.getPiece() is now tile.piece
    
    Also, when you select a tile, you now need to pass in a tile object, so it's no longer tile.select() and is instead tile.select(tile)

--- Chess/PieceLogic.py
+++ Chess/PieceLogic.py
@@ -1,81 +1,92 @@
-#Method to display movement options specific to the rook
-def rookCheck(isWhite, gameState, x, y):
+def rookCheck(piece, gameState):
+    isWhite = piece.isWhite
+    x = piece.x
+    y = piece.y
     for tileX in range(7-x):
         rightX = x + tileX + 1
-        if gameState[rightX][y].getPiece() == " ":
-            gameState[rightX][y].select()
-        elif gameState[rightX][y].getTeam() != isWhite:
-            gameState[rightX][y].select()
+        if gameState[rightX][y].piece == " ":
+            gameState[rightX][y].select(piece)
+        elif gameState[rightX][y].isWhite != isWhite:
+            gameState[rightX][y].select(piece)
             break
         else:
             break
     for tileX in range(x-1):
         leftX = x - tileX - 1
-        if gameState[leftX][y].getPiece() == " ":
-            gameState[leftX][y].select()
-        elif gameState[leftX][y].getTeam() != isWhite:
-            gameState[leftX][y].select()
+        if gameState[leftX][y].piece == " ":
+            gameState[leftX][y].select(piece)
+        elif gameState[leftX][y].isWhite != isWhite:
+            gameState[leftX][y].select(piece)
             break
         else:
             break
     
     for tileY in range(7-y):
         downY = y + tileY + 1
-        if gameState[x][downY].getPiece() == " ":
-            gameState[x][downY].select()
-        elif gameState[x][downY].getTeam() != isWhite:
-            gameState[x][downY].select()
+        if gameState[x][downY].piece == " ":
+            gameState[x][downY].select(piece)
+        elif gameState[x][downY].isWhite != isWhite:
+            gameState[x][downY].select(piece)
             break
         else:
             break
     if y - 1 >= 0:
         for tileY in range(y-1):
             upY = y - tileY - 1
-            if gameState[x][upY].getPiece() == " ":
-                gameState[x][upY].select()
-            elif gameState[x][upY].getTeam() != isWhite:
-                gameState[x][upY].select()
+            if gameState[x][upY].piece == " ":
+                gameState[x][upY].select(piece)
+            elif gameState[x][upY].isWhite != isWhite:
+                gameState[x][upY].select(piece)
                 break
             else:
                 break
             
-#Method to display movement options specific to the knight
-def knightCheck(isWhite, gameState, x, y):
+def knightCheck(piece, gameState):
+    
+    isWhite = piece.isWhite
+    x = piece.x
+    y = piece.y
+    
     if x+2 < 8 and y + 1 < 8:
-        if gameState[x+2][y+1].getTeam() is not isWhite or (gameState[x+2][y+1].getPiece() is " "):
-            gameState[x+2][y+1].select()
+        if gameState[x+2][y+1].isWhite is not isWhite or (gameState[x+2][y+1].piece is " "):
+            gameState[x+2][y+1].select(piece)
     if x+2 < 8 and y - 1 >= 0:
-        if gameState[x+2][y-1].getTeam() is not isWhite or (gameState[x+2][y-1].getPiece() is " "):
-            gameState[x+2][y-1].select()
+        if gameState[x+2][y-1].isWhite is not isWhite or (gameState[x+2][y-1].piece is " "):
+            gameState[x+2][y-1].select(piece)
     if x-2 >= 0 and y + 1 < 8:
-        if gameState[x-2][y+1].getTeam() is not isWhite or (gameState[x-2][y+1].getPiece() is " "):
-            gameState[x-2][y+1].select()
+        if gameState[x-2][y+1].isWhite is not isWhite or (gameState[x-2][y+1].piece is " "):
+            gameState[x-2][y+1].select(piece)
     if x-2 >= 0 and y - 1 >= 0:
-        if (gameState[x-2][y-1].getTeam() != isWhite) or (gameState[x-2][y-1].getPiece() == " "):
-            gameState[x-2][y-1].select()
+        if (gameState[x-2][y-1].isWhite != isWhite) or (gameState[x-2][y-1].piece == " "):
+            gameState[x-2][y-1].select(piece)
     if x+1 < 8 and y + 2 < 8:
-        if gameState[x+1][y+2].getTeam() is not isWhite or (gameState[x+1][y+2].getPiece() is " "):
-            gameState[x+1][y+2].select()
+        if gameState[x+1][y+2].isWhite is not isWhite or (gameState[x+1][y+2].piece is " "):
+            gameState[x+1][y+2].select(piece)
     if x+1 < 8 and y - 2 >= 0:
-        if gameState[x+1][y-2].getTeam() is not isWhite or (gameState[x+1][y-2].getPiece() is " "):
-            gameState[x+1][y-2].select()
+        if gameState[x+1][y-2].isWhite is not isWhite or (gameState[x+1][y-2].piece is " "):
+            gameState[x+1][y-2].select(piece)
     if x-1 >= 0 and y + 2 < 8:
-        if gameState[x-1][y+2].getTeam() is not isWhite or (gameState[x-1][y+2].getPiece() is " "):
-            gameState[x-1][y+2].select()
+        if gameState[x-1][y+2].isWhite is not isWhite or (gameState[x-1][y+2].piece is " "):
+            gameState[x-1][y+2].select(piece)
     if x-1 >= 0 and y - 2 >= 0:
-        if gameState[x-1][y-2].getTeam() is not isWhite or (gameState[x-1][y-2].getPiece() is " "):
-            gameState[x-1][x-2].select()
+        if gameState[x-1][y-2].isWhite is not isWhite or (gameState[x-1][y-2].piece is " "):
+            gameState[x-1][x-2].select(piece)
             
-def pawnCheck(isWhite, gameState, x, y):
+def pawnCheck(piece, gameState):
+    
+    isWhite = piece.isWhite
+    x = piece.x
+    y = piece.y
+    
     if not isWhite:
-        if gameState[x][y-1].getPiece() == " ":
-            gameState[x][y-1].select()
+        if gameState[x][y-1].piece == " ":
+            gameState[x][y-1].select(piece)
     else:
-        if gameState[x][y+1].getPiece() == " ":
-            gameState[x][y+1].select()
-    if y==6:
-        if gameState[x][y-2].getPiece() == " ":
-            gameState[x][y-2].select()
-    if y==1:
-        if gameState[x][y+2].getPiece() == " ":
-            gameState[x][y+2].select()
+        if gameState[x][y+1].piece == " ":
+            gameState[x][y+1].select(piece)
+    if y==6 and not isWhite:
+        if gameState[x][y-2].piece == " " and gameState[x][y-1].piece == " ":
+            gameState[x][y-2].select(piece)
+    if y==1 and isWhite:
+        if gameState[x][y+2].piece == " " and gameState[x][y+1].piece == " ":
+            gameState[x][y+2].select(piece)
