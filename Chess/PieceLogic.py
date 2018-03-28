29461a6cd34bf1bee727e4bd6a6533094339bb90
Rheemster <31289850+Rheemster@users.noreply.github.com>
2018-03-27 20:34:33 -0600

    Update PieceLogic.py

--- Chess/PieceLogic.py
+++ Chess/PieceLogic.py
@@ -1,23 +1,28 @@
+import Tile
+
 #Raheem Nimnicht
-def rookCheck(piece, gameState):
-    isWhite = piece.isWhite
-    x = piece.x
-    y = piece.y
+def rookCheck(tile, gameState):
+    isWhite = tile.isWhite
+    x = tile.x
+    y = tile.y
+    nullTile = Tile.Tile(-1,-1)
+    tileList = [nullTile]
+    
     for tileX in range(7-x):
         rightX = x + tileX + 1
         if gameState[rightX][y].piece == " ":
-            gameState[rightX][y].select(piece)
+            tileList.append(gameState[rightX][y])
         elif gameState[rightX][y].isWhite != isWhite:
-            gameState[rightX][y].select(piece)
+            tileList.append(gameState[rightX][y])
             break
         else:
             break
     for tileX in range(x):
         leftX = x - tileX - 1
         if gameState[leftX][y].piece == " ":
-            gameState[leftX][y].select(piece)
+            tileList.append(gameState[leftX][y])
         elif gameState[leftX][y].isWhite != isWhite:
-            gameState[leftX][y].select(piece)
+            tileList.append(gameState[leftX][y])
             break
         else:
             break
@@ -25,9 +30,9 @@ def rookCheck(piece, gameState):
     for tileY in range(7-y):
         downY = y + tileY + 1
         if gameState[x][downY].piece == " ":
-            gameState[x][downY].select(piece)
+            tileList.append(gameState[x][downY])
         elif gameState[x][downY].isWhite != isWhite:
-            gameState[x][downY].select(piece)
+            tileList.append(gameState[x][downY])
             break
         else:
             break
@@ -35,12 +40,15 @@ def rookCheck(piece, gameState):
         for tileY in range(y):
             upY = y - tileY - 1
             if gameState[x][upY].piece == " ":
-                gameState[x][upY].select(piece)
+                tileList.append(gameState[x][upY])
             elif gameState[x][upY].isWhite != isWhite:
-                gameState[x][upY].select(piece)
+                tileList.append(gameState[x][upY])
                 break
             else:
                 break
+    
+    del tileList[0]
+    return tileList
             
 #Raheem Nimnicht
 def knightCheck(tile, gameState):
@@ -48,145 +56,152 @@ def knightCheck(tile, gameState):
     isWhite = tile.isWhite
     x = tile.x
     y = tile.y
+    nullTile = Tile.Tile(-1,-1)
+    tileList = [nullTile]
     
     if x+2 < 8 and y + 1 < 8: #Right 2, Down 1
         if gameState[x+2][y+1].isWhite is not isWhite or (gameState[x+2][y+1].piece == " "):
-            gameState[x+2][y+1].select(tile)
+            tileList.append(gameState[x+2][y+1])
     if x+2 < 8 and y - 1 >= 0: #Right 2, Up 1
         if gameState[x+2][y-1].isWhite is not isWhite or (gameState[x+2][y-1].piece == " "):
-            gameState[x+2][y-1].select(tile)
+            tileList.append(gameState[x+2][y-1])
     if x-2 >= 0 and y + 1 < 8: #Left 2, Down 1
         if gameState[x-2][y+1].isWhite is not isWhite or (gameState[x-2][y+1].piece == " "):
-            gameState[x-2][y+1].select(tile)
+            tileList.append(gameState[x-2][y+1])
     if x-2 >= 0 and y - 1 >= 0: #Left 2, Up 1
         if (gameState[x-2][y-1].isWhite != isWhite) or (gameState[x-2][y-1].piece == " "):
-            gameState[x-2][y-1].select(tile)
+            tileList.append(gameState[x-2][y-1])
     if x+1 < 8 and y + 2 < 8: #Right 1, Down 2
         if gameState[x+1][y+2].isWhite is not isWhite or (gameState[x+1][y+2].piece == " "):
-            gameState[x+1][y+2].select(tile)
+            tileList.append(gameState[x+1][y+2])
     if x+1 < 8 and y - 2 >= 0: #Right 1, Up 2
         if gameState[x+1][y-2].isWhite is not isWhite or (gameState[x+1][y-2].piece == " "):
-            gameState[x+1][y-2].select(tile)
+            tileList.append(gameState[x+1][y-2])
     if x-1 >= 0 and y + 2 < 8: #Left 1, Down 2
         if gameState[x-1][y+2].isWhite is not isWhite or (gameState[x-1][y+2].piece == " "):
-            gameState[x-1][y+2].select(tile)
+            tileList.append(gameState[x-1][y+2])
     if x-1 >= 0 and y - 2 >= 0: #Left 1, Up 2
         if gameState[x-1][y-2].isWhite is not isWhite or (gameState[x-1][y-2].piece == " "):
-            gameState[x-1][y-2].select(tile)
+            tileList.append(gameState[x-1][y-2])
+    
+    del tileList[0]
+    return tileList
             
 #David Kang
-def pawnCheck(piece, gameState):
+def pawnCheck(tile, gameState):
     
-    isWhite = piece.isWhite
-    x = piece.x
-    y = piece.y
+    isWhite = tile.isWhite
+    x = tile.x
+    y = tile.y
+    nullTile = Tile.Tile(-1,-1)
+    tileList = [nullTile]
     
     if isWhite:
         if gameState[x][y-1].piece == " ":
-            gameState[x][y-1].select(piece)
+            tileList.append(gameState[x][y-1])
         if x - 1 >= 0 and y - 1 >= 0:
             if gameState[x-1][y-1].piece != " " and gameState[x-1][y-1].isWhite != isWhite:
-                gameState[x-1][y-1].select(piece)
+                tileList.append(gameState[x-1][y-1])
         if x + 1 < 8 and y - 1 >= 0:
             if gameState[x+1][y-1].piece != " " and gameState[x+1][y-1].isWhite != isWhite:
-                gameState[x+1][y-1].select(piece)
+                tileList.append(gameState[x+1][y-1])
     #white
     elif y+1 < 8:
         if gameState[x][y+1].piece == " ":
-            gameState[x][y+1].select(piece)
+            tileList.append(gameState[x][y+1])
         if x - 1 >= 0 and y + 1 < 8:
             if gameState[x-1][y+1].piece != " " and gameState[x-1][y+1].isWhite != isWhite:
-                gameState[x-1][y+1].select(piece)
+                tileList.append(gameState[x-1][y+1])
         if x + 1 < 8 and y + 1 < 8:
             if gameState[x+1][y+1].piece != " " and gameState[x+1][y+1].isWhite != isWhite:
-                gameState[x+1][y+1].select(piece)
+                tileList.append(gameState[x+1][y+1])
             
     if y==6 and isWhite:
         if gameState[x][y-2].piece == " " and gameState[x][y-1].piece == " ":
-            gameState[x][y-2].select(piece)
+            tileList.append(gameState[x][y-2])
     #Black
     if y==1 and not isWhite:
         if gameState[x][y+2].piece == " " and gameState[x][y+1].piece == " ":
-            gameState[x][y+2].select(piece)
+            tileList.append(gameState[x][y+2])
 #the code below is to check weather you got a rook to the end of the board, this turns that rook into a king.--Bella and Raheem
     if y==7 and not isWhite:
-        piece.newPiece("Queen",isWhite)
+        tile.newPiece("Queen",isWhite)
     if y==0 and isWhite:
-        piece.newPiece("Queen",isWhite)
+        tile.newPiece("Queen",isWhite)
         
-def kingCheck(piece, gameState):
-    isWhite = piece.isWhite
-    x = piece.x
-    y = piece.y
-# comments in king refer to code below--Bella
-    # I want to check for out of bounds
-# this if statement is checking what team the piece is on,
-# if it is a white team than the piece can move up one, if it is a black team it can move down one
-# the if statement that look like this: **if y-1 > 7 and y-1 < 7:** it is to check and make sure things are not out of bounds
-# the if statement that looks like this: **gameState[x][y-1].isWhite** is checking if a tile near
-# it is the other team, if it is, it gets highlighted
-    # while y+1 == 0-7 or y-1 == 0-7 or x+1 == 0-7 or x-1 == 0-7:
-    # if not gameState[y][x].y > 6:#outofbounds exeption right 
-    #     gameState[x+1][y].select(piece)    
+    del tileList[0]
+    return tileList
+        
+def kingCheck(tile, gameState):
+    isWhite = tile.isWhite
+    x = tile.x
+    y = tile.y
+    nullTile = Tile.Tile(-1,-1)
+    tileList = [nullTile]
     
     if isWhite:
         if not gameState[y][x].x  < 1:#outofbounds exeption up
             if gameState[x][y-1].piece == " " or not gameState[x][y-1].isWhite:
-                gameState[x][y-1].select(piece)
+                tileList.append(gameState[x][y-1])
         if not gameState[y][x].x  > 6:#outofbounds exeption down
         
             if gameState[x][y+1].piece == " " or not gameState[x][y+1].isWhite:
         
-                gameState[x][y+1].select(piece)
+                tileList.append(gameState[x][y+1])
         if not gameState[y][x].y > 6:#outofbounds exeption right
             if gameState[x+1][y].piece == " " or not gameState[x+1][y].isWhite:
-                gameState[x+1][y].select(piece)
+                tileList.append(gameState[x+1][y])
             if not gameState[y][x].x  > 6:#outofbounds exeption down
                 if gameState[x+1][y+1].piece == " " or not gameState[x+1][y+1].isWhite:
-                    gameState[x+1][y+1].select(piece)
+                    tileList.append(gameState[x+1][y+1])
             if not gameState[y][x].x  < 1:#outofbounds exeption up
                 if gameState[x+1][y-1].piece == " " or not gameState[x+1][y-1].isWhite:
-                    gameState[x+1][y-1].select(piece)
+                    tileList.append(gameState[x+1][y-1])
         if  not gameState[y][x].y < 1:#this line of code is making sure that their is not an outofbounds exeption for going left on the board.
             if gameState[x-1][y].piece == " " or not gameState[x-1][y].isWhite:
-                gameState[x-1][y].select(piece)
+                tileList.append(gameState[x-1][y])
             if not gameState[y][x].x  > 6:#outofbounds exeption down
                 if gameState[x-1][y+1].piece == " " or not gameState[x-1][y+1].isWhite:
-                    gameState[x-1][y+1].select(piece)
+                    tileList.append(gameState[x-1][y+1])
             if not gameState[y][x].x  < 1:#outofbounds exeption up
                 if gameState[x-1][y-1].piece == " " or not gameState[x-1][y-1].isWhite:
-                    gameState[x-1][y-1].select(piece)
+                    tileList.append(gameState[x-1][y-1])
     else:
         if not gameState[y][x].x  < 1:#outofbounds exeption up
             if gameState[x][y-1].piece == " " or gameState[x][y-1].isWhite:
-                gameState[x][y-1].select(piece)
+                tileList.append(gameState[x][y-1])
         if not gameState[y][x].x  > 6:#outofbounds exeption down
             if gameState[x][y+1].piece == " " or gameState[x][y+1].isWhite:
-                gameState[x][y+1].select(piece)
+                tileList.append(gameState[x][y+1])
             if not gameState[y][x].y > 6:#outofbounds exeption right
                 if gameState[x+1][y].piece == " " or gameState[x+1][y].isWhite:
-                    gameState[x+1][y].select(piece)
+                    tileList.append(gameState[x+1][y])
                 if not gameState[y][x].x  > 6:#outofbounds exeption down
                     if gameState[x+1][y+1].piece == " " or gameState[x+1][y+1].isWhite:
-                        gameState[x+1][y+1].select(piece)
+                        tileList.append(gameState[x+1][y+1])
                 if not gameState[y][x].x  < 1:#outofbounds exeption up
                     if gameState[x+1][y-1].piece == " " or gameState[x+1][y-1].isWhite:
-                        gameState[x+1][y-1].select(piece)
+                        tileList.append(gameState[x+1][y-1])
         if  not gameState[y][x].y < 1:#this line of code is making sure that their is not an outofbounds exeption for going left on the board.
             if gameState[x-1][y].piece == " " or gameState[x-1][y].isWhite:
-                gameState[x-1][y].select(piece)
+                tileList.append(gameState[x-1][y])
             if not gameState[y][x].x  > 6:#outofbounds exeption down
                 if gameState[x-1][y+1].piece == " " or gameState[x-1][y+1].isWhite:
-                    gameState[x-1][y+1].select(piece)
+                    tileList.append(gameState[x-1][y+1])
             if not gameState[y][x].x  < 1:#outofbounds exeption up
                 if gameState[x-1][y-1].piece == " " or gameState[x-1][y-1].isWhite:
-                    gameState[x-1][y-1].select(piece)
+                    tileList.append(gameState[x-1][y-1])
+                    
+    del tileList[0]
+    return tileList
             
 def bishopCheck(tile, gameState):
     
     isWhite = tile.isWhite
     x = tile.x
     y = tile.y
+    nullTile = Tile.Tile(-1,-1)
+    tileList = [nullTile]
     
     tilesLeft = x
     tilesUp = y
@@ -202,9 +217,9 @@ def bishopCheck(tile, gameState):
         xSpace = x+i+1
         ySpace = y+i+1
         if gameState[xSpace][ySpace].piece == " ":
-            gameState[xSpace][ySpace].select(tile)
+            tileList.append(gameState[xSpace][ySpace])
         elif gameState[xSpace][ySpace].isWhite is not isWhite:
-            gameState[xSpace][ySpace].select(tile)
+            tileList.append(gameState[xSpace][ySpace])
             break
         else:
             break
@@ -214,14 +229,13 @@ def bishopCheck(tile, gameState):
         maxTiles = tilesUp
     else:
         maxTiles = tilesLeft
-    print(maxTiles)
     for i in range(maxTiles):
         xSpace = x-i-1
         ySpace = y-i-1
         if gameState[xSpace][ySpace].piece == " ":
-            gameState[xSpace][ySpace].select(tile)
+            tileList.append(gameState[xSpace][ySpace])
         elif gameState[xSpace][ySpace].isWhite is not isWhite:
-            gameState[xSpace][ySpace].select(tile)
+            tileList.append(gameState[xSpace][ySpace])
             break
         else:
             break
@@ -234,9 +248,9 @@ def bishopCheck(tile, gameState):
         xSpace = x-i-1
         ySpace = y+i+1
         if gameState[xSpace][ySpace].piece == " ":
-            gameState[xSpace][ySpace].select(tile)
+            tileList.append(gameState[xSpace][ySpace])
         elif gameState[xSpace][ySpace].isWhite is not isWhite:
-            gameState[xSpace][ySpace].select(tile)
+            tileList.append(gameState[xSpace][ySpace])
             break
         else:
             break
@@ -249,9 +263,12 @@ def bishopCheck(tile, gameState):
         xSpace = x+i+1
         ySpace = y-i-1
         if gameState[xSpace][ySpace].piece == " ":
-            gameState[xSpace][ySpace].select(tile)
+            tileList.append(gameState[xSpace][ySpace])
         elif gameState[xSpace][ySpace].isWhite is not isWhite:
-            gameState[xSpace][ySpace].select(tile)
+            tileList.append(gameState[xSpace][ySpace])
             break
         else:
-            break    
+            break
+        
+    del tileList[0]
+    return tileList
